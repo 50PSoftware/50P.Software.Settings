46250bbf591c5e4cd65fc95bf7cc20f27073db4b
Adaraneo <35331052+Adaraneo@users.noreply.github.com>
2022-05-29 19:05:33 +0200

    Add files via upload

---
+++ SettingsDialog/Settings.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50P.Software.Settings.Dialogs
+{
+    public class Settings : ApplicationSettingsBase
+    {
+        [UserScopedSetting()]
+        [DefaultSettingValue("false")]
+        public bool UseDatabase
+        {
+            get
+            {
+                return (bool)this["UseDatabase"];
+            }
+            set
+            {
+                this["UseDatabase"] = (bool)value;
+            }
+        }
+
+        [UserScopedSetting()]
+        [DefaultSettingValue(null)]
+        public string Filename
+        {
+            get
+            {
+                return (string)this["Filename"];
+            }
+            set
+            {
+                this["Filename"] = (string)value;
+            }
+        }
+
+        [UserScopedSetting()]
+        [DefaultSettingValue(null)]
+        public string Server
+        {
+            get
+            {
+                return (string)this["Server"];
+            }
+            set
+            {
+                this["Server"] = (string)value;
+            }
+        }
+
+        [UserScopedSetting()]
+        [DefaultSettingValue(null)]
+        public string DatabaseName
+        {
+            get
+            {
+                return (string)this["DatabaseName"];
+            }
+            set
+            {
+                this["DatabaseName"] = (string)value;
+            }
+        }
+
+        [UserScopedSetting()]
+        [DefaultSettingValue(null)]
+        public string Username
+        { 
+            get
+            {
+                return (string)this["Username"];
+            }
+            set
+            {
+                this["Username"] = (string)value;
+            }
+        }
+
+        [UserScopedSetting()]
+        [DefaultSettingValue(null)]
+        public byte[] Password
+        {
+            get
+            {
+                return (byte[])this["Password"];
+            }
+            set
+            {
+                this["Password"] = (byte[])value;
+            }
+        }
+
+        [UserScopedSetting()]
+        [DefaultSettingValue(null)]
+        public string FileExtension
+        {
+            get
+            {
+                return (string)this["FileExtension"];
+            }
+            set
+            {
+                this["FileExtension"] = (string)value;
+            }
+        }
+    }
+}
